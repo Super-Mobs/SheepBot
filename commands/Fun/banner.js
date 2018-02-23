32da1e99659d677786f535dcabc2820952680d3c
Super_Sheep <emersont2003@gmail.com>
2018-02-23 16:39:14 -0500

    CoolDown Function

---
+++ commands/Fun/banner.js
@@ -0,0 +1,24 @@
+const figletAsync = require("util").promisify(require("figlet"));
+
+exports.run = async (client, msg, [banner]) => {
+  const data = await figletAsync(banner);
+  return msg.channel.send(data, { code: true });
+};
+exports.conf = {
+  enabled: true,
+  selfbot: false,
+  runIn: ["text", "dm", "group"],
+  aliases: [],
+  permLevel: 0,
+  botPerms: [],
+  requiredFuncs: [],
+  requiredModules: ["figlet"],
+};
+
+exports.help = {
+  name: "banner",
+  description: "Creates an ASCII banner from the string you supply",
+  usage: "<banner:str>",
+  usageDelim: "",
+  type: "commands",
+};
